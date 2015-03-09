a22f2ff6825eb9a9b2434c74658191771f077f01
Seth Lyons <slyons@fxcm.com>
2015-03-09 20:33:28 +0000

    add kernel support for carp to freebsd if needed and do a better job supporting carp on freebsd 10+

--- manifests/network/carp.pp
+++ manifests/network/carp.pp
@@ -5,6 +5,7 @@
 class bsd::network::carp (
   $allowed = true,
   $preempt = false,
+  $kernmod = false,
 ) {
 
   if $allowed == true {
@@ -17,5 +18,26 @@ class bsd::network::carp (
     sysctl::value { 'net.inet.carp.preempt': value => '1' }
   } else {
     sysctl::value { 'net.inet.carp.preempt': value => '0' }
+  }
+
+  # if kernmod is true, add carp_load="YES" to /boot/loader.conf
+  # and run kldload carp
+  if $kernmod == true {
+    case $::osfamily {
+      'FreeBSD': {
+        file_line { 'carp_load_line':
+          path => '/boot/loader.conf',
+          line => 'carp_load="YES"',
+        }
+
+        exec { 'load_carp':
+          command => '/sbin/kldload carp',
+          unless  => '/sbin/kldstat -n carp.ko',
+        }
+      }
+      default: {
+        fail("${::osfamily} does not need to load this kernel module")
+      }
+    }
   }
 }
--- spec/defines/bsd_network_interface_carp_spec.rb
+++ spec/defines/bsd_network_interface_carp_spec.rb
@@ -41,4 +41,62 @@ describe "bsd::network::interface::carp" do
       }.to raise_error(Puppet::Error, /does not match/)
     end
   end
+
+  context "on FreeBSD 10" do
+    let(:facts) {
+      {
+        :kernel           => 'FreeBSD',
+        :osfamily         => 'FreeBSD',
+        :kernelmajversion => '10',
+      }
+    }
+    let(:title) { 'em0_alias0' }
+    let(:params) {
+      {
+        :id      => '1',
+        :device  => 'em0',
+        :address => '10.0.0.1/24',
+        :advbase => '1',
+        :advskew => '0',
+        :pass    => 'TopSecret'
+      }
+    }
+    it "should allow device name em0_alias0" do
+      should contain_bsd__network__interface__carp('em0_alias0')
+    end
+  end
+
+  context "on FreeBSD 9" do
+    let(:facts) {
+      {
+        :kernel           => 'FreeBSD',
+        :osfamily         => 'FreeBSD',
+        :kernelmajversion => '9',
+      }
+    }
+    let(:params) {
+      {
+        :id      => '1',
+        :device  => 'em0',
+        :address => '10.0.0.1/32',
+        :advbase => '1',
+        :advskew => '0',
+        :pass    => 'TopSecret',
+      }
+    }
+    context "with name carp0" do
+      let(:title) { 'carp0' }
+      it "should have device name carp0" do
+        should contain_bsd__network__interface__carp
+      end
+    end
+    context "with name em0_alias0" do
+      let(:title) { 'em0_alias0' }
+      it "should fail because name must include carp" do
+        expect {
+          should contain_bsd__network__interface__carp('em0_alias0')
+        }.to raise_error(Puppet::Error, /does not match/)
+      end
+    end
+  end
 end
