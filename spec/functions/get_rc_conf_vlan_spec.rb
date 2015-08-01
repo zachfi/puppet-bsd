9ba508087348dbef51ea1470cd813af360610b7f
Zach Leslie <zach@puppetlabs.com>
2015-07-31 19:24:33 -0700

    Add bassing spec for basic vlan configuration

---
+++ spec/functions/get_rc_conf_vlan_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'get_rc_conf_vlan' do
+  context 'with a simple config' do
+    desired = ["vlan 1", "vlandev em0"]
+
+    config = {
+      "name"        => 'vlan1',
+      "description" => "Trees",
+      "device"      => 'em0',
+      "id"          => '1',
+      "address"     => [
+        '10.0.1.12/24',
+      ],
+    }
+    it { should run.with_params(config).and_return(desired) }
+  end
+end
